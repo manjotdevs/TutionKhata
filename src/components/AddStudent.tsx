94c51c18b1fe5120c1e2ddbfac2fe5981081f046
manjotdevs <manjotdevs459@gmail.com>
2024-06-01 23:38:13 +0530

    student is added

---
+++ src/components/AddStudent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Avatar, Card, IconButton } from 'react-native-paper';
+
+function AddStudent(){
+    return(
+        <Card>
+            <Card.Title title="Add Student" />
+            <Card.Content>
+                <IconButton icon="plus" />
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default AddStudent;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import AppBar from '../components/AppBar';
+import AddStudent from '../components/AddStudent';
 
 function Home() {
-  return(
-    <AppBar />
-
-  )
+  return (
+    <>
+      <AppBar />
+      <AddStudent />
+    </>
+  );
 }
-
 
 export default Home;
