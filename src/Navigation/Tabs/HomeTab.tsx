6762f32ecb2d590de66e80023329a747cc0407e8
manjotdevs <manjotdevs459@gmail.com>
2024-06-20 14:20:56 +0530

    UI is imprioved

--- src/Navigation/Tabs/HomeTab.tsx
+++ src/Navigation/Tabs/HomeTab.tsx
@@ -1,24 +1,30 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { useColorScheme } from 'react-native';
 import Home from '../../Screens/Home';
 import Main from '../../Screens/Main';
-import {Background,Text} from '../../utils/Themes';
+import {Background,Custom} from '../../utils/Themes';
 import {SView} from '../../utils/Styled';
 
 function HomeTab(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
+  const colorScheme = useColorScheme();
   return (
     <>
       <SView className={`flex-1 ${Background}`}>
-        <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+        <Stack.Navigator
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: { backgroundColor: colorScheme==='dark'?Custom.BackgroundDark:Custom.BackgroundLight},
+          }}>
           <Stack.Screen
             name="StackHomeScreen"
             component={Home}
             options={{
               headerShown: true,
-              headerTitle:'Home',
-              headerStyle:{backgroundColor: Background.primary},
-              headerTitleStyle:{color: Text.primary}
-              
+              headerTitle: 'Home',
+              headerBackButtonMenuEnabled: true,
+              headerTransparent: true,
+              headerTintColor: colorScheme==='dark'?Custom.TextDark:Custom.TextLight,
             }}
           />
           <Stack.Screen name="Main" component={Main} />
--- src/utils/Themes.tsx
+++ src/utils/Themes.tsx
@@ -1,15 +1,22 @@
+export const Custom = {
+  BackgroundLight: 'rgb(250 250 250)',
+  BackgroundDark: 'rgb(3 7 18)',
+  TextLight: 'rgba(3 718)',
+  TextDark: 'rgba(250 250 250)', 
+};
+
 export const Background = {
-  primary: 'bg-neutral-50 text-white dark:bg-gray-950',
+  primary: 'bg-neutral-50 dark:bg-black',
 };
+
 export const Text = {
-  primary: 'text-red-400',
+  primary: 'text-black dark:text-white',
 };
-
-
 
 export const Button = {
   primary: 'bg-blue-400 text-white dark:bg-blue-600',
   secondary: '',
-  floating:'bg-blue-500 w-16 h-16 ml-[298] relative rounded-full flex items-center justify-center shadow-custom',
+  floating:
+    'bg-blue-500 w-16 h-16 ml-[298] relative rounded-full flex items-center justify-center shadow-custom',
   unactive: '',
 };
