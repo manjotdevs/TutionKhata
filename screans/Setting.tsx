8f50c2ffcd741d6a53d2618875f5d7ff84f1c26a
Manjot <mssond459@gmail.com>
2024-07-12 16:47:02 +0530

    library update

--- screans/Setting.tsx
+++ screans/Setting.tsx
@@ -1,11 +1,17 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView,Button,Image, StyleSheet } from "react-native";
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
+
 
 export default function Setting() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Setting</Text>
+         <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
       </View>
     </SafeAreaView>
   );
 }
+
